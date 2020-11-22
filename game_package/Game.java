1806c22c858f3bcb0cfa786a1500929b7dbe652c
Louis Brejon <louis.brejon@enseirb-matmeca.fr>
2020-11-22 16:40:31 +0100

    Gestion des tours (player/IA/player/..)

--- game_package/Game.java
+++ game_package/Game.java
@@ -1,5 +1,9 @@
 package game_package;
 
+import java.io.Console;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game{
 
     // Attributs
@@ -37,9 +41,86 @@ public class Game{
         return player2;
     }
 
+    // Return which player plays
+    public static int which_player(int i){
+      int res = i%2;
+      if (res == 0)
+        return 2;
+      else
+        return 1;
+    }
+
+    // Return a random number
+    public static int getRandomNumber(int min, int max) throws IllegalArgumentException {
+      if (min > max)
+        throw new IllegalArgumentException("Max must be greater than min");
+
+      Random nb = new Random();
+      return nb.nextInt((max-min)+1)+min;
+    }
+
+    // Update the grid after a turn
+    public static void update_grid(int[][] grid, int position, int player){
+      player = which_player(player);            // define which is player's turn (player 1 or player 2 ?)
+
+      for(int k = 0; k<6; k++){
+        if (grid[5-k][position-1] == 0){        // if cell [position][k] is empty
+          if (player == 1)
+            grid[5-k][position-1] = -1;
+          else
+            grid[5-k][position-1] = 1;
+          int abs = 5-k+1;
+          int ord = position-1+1;
+          // System.out.println("Player "+player+" joue en position ("+abs+","+ord+")\n");
+          // return grid;
+          break;
+        }
+      }
+
+      // System.out.println("Error : column is full. Please choose another column");
+
+      // return grid;
+    }
+
+
     // Méthodes
     public void play(){
+        int i = 1; // player 1 starts playing
+        int position;
+        int win1 = 0, win2 = 0;
+        String buffer;
+        Console console = System.console();
+
         interface_package.Display.display_grid(getGrid().values);
+
+        while(win1 != 1 || win2 != 1){
+             i = which_player(i);
+
+             if (i == 1){
+               buffer = console.readLine();
+
+               // if (buffer.equals("sortir")){
+               //   System.out.println("You quit the game");
+               //   System.exit(0);
+               // }
+               // else{
+                 position = Integer.parseInt(buffer);
+                 while (position < 1 || position > 7){
+                   System.out.print("Wrong position. Please choose a number from 1 to 7 : ");
+                   // position = position.next();
+                   position = Integer.parseInt(console.readLine());  // rajouter des conditions de test
+                   System.out.println("");
+                 }
+               // }
+             }
+             else
+               position = getRandomNumber(1,7);
+
+             System.out.println("");
+             update_grid(getGrid().values, position, i);
+             interface_package.Display.display_grid(getGrid().values);
+             i++;
+           }
     }
 
   public static void main(String[] args) {
